27d1b0872e06c47fe2001de24c3a67d0d6b8ef54
Joshua Yun Pei NIKIEMA <nikiemajoshua24@gmail.com>
2025-07-24 19:55:44 +0000

    Corriger l'import TensorFlow.js platform

--- components/LocalModelService.ts
+++ components/LocalModelService.ts
@@ -1,6 +1,5 @@
 import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-react-native';
-import '@tensorflow/tfjs-platform-react-native';
 import { manipulateAsync, FlipType, SaveFormat } from 'expo-image-manipulator';
 import { Asset } from 'expo-asset';
 import i18n from '@/utils/i18n';
