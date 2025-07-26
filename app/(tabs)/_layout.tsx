4f7218c2ea241ac373b43ac583a535d04d87b7cc
Joshua Yun Pei NIKIEMA <nikiemajoshua24@gmail.com>
2025-07-26 01:29:51 +0000

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Camera, History, Settings, User } from 'lucide-react-native';
+import { Chrome as Home, Camera, History, Settings, User } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
